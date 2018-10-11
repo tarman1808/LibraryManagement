bba6daf1f2d7bbe0683e67ca85ee5850a0c08c69
tarman1808 <tarman1808@gmail.com>
2018-10-11 20:16:23 +0700

    initial commit

--- src/library/management/ui/addbook/AddbookController.java
+++ src/library/management/ui/addbook/AddbookController.java
@@ -1,4 +1,26 @@
 package library.management.ui.addbook;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
 public class AddbookController {
+    
+    @FXML
+    private JFXTextField booktitletf;
+
+    @FXML
+    private JFXTextField bookidtf;
+
+    @FXML
+    private JFXTextField bookauthortf;
+
+    @FXML
+    private JFXTextField publishertf;
+
+    @FXML
+    private JFXButton saveaddbook;
+
+    @FXML
+    private JFXButton canceladdbook;
 }
